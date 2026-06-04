97be89fbcd6ca6980e0fbd369b53dbe2a80984af
Aadish7 <159457482+Aadish7@users.noreply.github.com>
2026-06-04 17:18:17 +0530

    Update github_test.py

--- github_test.py
+++ github_test.py
@@ -1,1 +1,20 @@
+import os
+import requests
+from dotenv import load_dotenv
 
+load_dotenv()
+
+token = os.getenv("GITHUB_TOKEN")
+
+headers = {
+    "Authorization": f"Bearer {token}",
+    "Accept": "application/vnd.github+json"
+}
+
+response = requests.get(
+    "https://api.github.com/user",
+    headers=headers
+)
+
+print("Status:", response.status_code)
+print(response.json())
